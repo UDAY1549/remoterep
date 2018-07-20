3b6f1c0493ce4ce99de0d257ed35ebf71bd79912
uday <uday1549@gmail.com>
2018-07-20 16:57:47 +0530

    add setter and getter methods to carttype class  by uday

--- GitProj/src/com/nt/dev/CardType.java
+++ GitProj/src/com/nt/dev/CardType.java
@@ -1,11 +1,26 @@
 package com.nt.dev;
 
 public class CardType {
-	
+
 	String MasterCard;
 	String VissaCard;
-	
-	
-	
+
+	// generate getter and setter methods
+
+	public String getMasterCard() {
+		return MasterCard;
+	}
+
+	public void setMasterCard(String masterCard) {
+		MasterCard = masterCard;
+	}
+
+	public String getVissaCard() {
+		return VissaCard;
+	}
+
+	public void setVissaCard(String vissaCard) {
+		VissaCard = vissaCard;
+	}
 
 }
