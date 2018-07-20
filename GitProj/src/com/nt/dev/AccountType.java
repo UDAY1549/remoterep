5b98af309235bdaaf3713b08174cb5770c622c8d
uday <uday@192.168.0.10>
2018-07-20 10:56:05 +0530

    created setter and getter methods by uday

--- GitProj/src/com/nt/dev/AccountType.java
+++ GitProj/src/com/nt/dev/AccountType.java
@@ -1,10 +1,26 @@
 package com.nt.dev;
 
 public class AccountType {
-	
+
 	String SavingAccount;
 	String CurrentAccount;
-	
-	
+
+	public String getSavingAccount() {
+		return SavingAccount;
+	}
+
+	public void setSavingAccount(String savingAccount) {
+		SavingAccount = savingAccount;
+	}
+
+	public String getCurrentAccount() {
+		return CurrentAccount;
+	}
+
+	public void setCurrentAccount(String currentAccount) {
+		CurrentAccount = currentAccount;
+	}
+
+	// create setter and getter methods
 
 }
