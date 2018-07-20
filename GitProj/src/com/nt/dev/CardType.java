ca4c42571f42e134172da7684769e0dcb5b72f2c
uday <uday1549@gmail.com>
2018-07-20 17:14:21 +0530

    add setter and getter methods into CT class by varam for conflict errors

--- GitProj/src/com/nt/dev/CardType.java
+++ GitProj/src/com/nt/dev/CardType.java
@@ -4,8 +4,23 @@ public class CardType {
 
 	String MasterCard;
 	String VissaCard;
-
+	
 	// generate getter and setter methods
+	
+	public String getMasterCard() {
+		return MasterCard;
+	}
+	public void setMasterCard(String masterCard) {
+		MasterCard = masterCard;
+	}
+	public String getVissaCard() {
+		return VissaCard;
+	}
+	public void setVissaCard(String vissaCard) {
+		VissaCard = vissaCard;
+	}
+
+	
 
 	
 }
